78aa807910e2fad53bb9225fae88351274887021
Brian Semrau <brian.semrau@gmail.com>
2021-05-19 11:04:50 -0400

    Add configurable limit to Repeater

--- addons/godot-behavior-tree-plugin/decorator/repeater.gd
+++ addons/godot-behavior-tree-plugin/decorator/repeater.gd
@@ -6,11 +6,18 @@ class_name BehaviorRepeater, "repeat_icon.png"
 # This node ignores failed and OK responses, choosing to retick the node instead
 
 
+# Whether there should be a limitted number of attempts before succeeding
+export(bool) var limit_repeats: bool = false
+# The maximum number of execution attempts per tick (if limit enabled)
+export(int) var max_attempts: int = 3
+
+
 func tick(tick: Tick) -> int:
 	# 0..1 children
 	for child in get_children():
-		
-		while true:
+		var attempts: int = 0
+		while not (limit_repeats and attempts < max_attempts):
+			attempts += 1
 			if child.tick(tick) == ERR_BUSY:
 				return ERR_BUSY
 	
